32189f82855f8ed05063f2489d4ad7f42c79c87d
AaronWisneski <awisneski@ltu.edu>
2023-09-21 15:12:22 -0400

    MainMenu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartGame(string sceneName)
+    {
+        Debug.Log("start");
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Exit()
+    {
+        Debug.Log("exit");
+    }
+}
